c79d88ebbf63920f3db802e5581022ffe0512b6f
Ion Cerezo <icerezo@birt.eus>
2026-01-21 15:12:35 +0100

    primeras cosas

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ProductList } from './compoments/product-list/product-list';
+import { ProductDetail } from './compoments/product-detail/product-detail';
+import { ProductForm } from './compoments/product-form/product-form';
+import { NotFound } from './compoments/not-found/not-found';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    ProductList,
+    ProductDetail,
+    ProductForm,
+    NotFound
   ],
   imports: [
     BrowserModule,
